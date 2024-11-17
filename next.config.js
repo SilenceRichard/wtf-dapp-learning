ba6e4d07246a1ccc6ddaf0e16dbe87bd4a22acf1
SilenceRichard <silencerichard@163.com>
2024-11-17 23:15:05 +0800

    feat: 🎸 调用ETH主网智能合约

--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: [
+    "@ant-design",
+    "antd",
+    "rc-util",
+    "rc-pagination",
+    "rc-picker",
+    "rc-input",
+  ],
+  webpack: (config) => {
+    config.cache = {
+      type: "filesystem",
+    };
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
