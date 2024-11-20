762253fbe62dacae0bda8ac3ad26cbc1d2f2783b
SilenceRichard <silencerichard@163.com>
2024-11-20 19:35:18 +0800

    feat: 🎸 添加hardhat合约

--- app/web3/page.tsx
+++ app/web3/page.tsx
@@ -1,140 +1,70 @@
-"use client";
+import { mainnet, sepolia, polygon, hardhat } from "wagmi/chains";
 import {
-  Mainnet,
-  Localhost,
-  Sepolia,
   WagmiWeb3ConfigProvider,
   MetaMask,
+  Sepolia,
+  Polygon,
+  Hardhat,
+  WalletConnect,
 } from "@ant-design/web3-wagmi";
-import { parseEther } from "viem";
-import { Button, message } from "antd";
-import {
-  http,
-  useReadContract,
-  useWatchContractEvent,
-  useWriteContract,
-} from "wagmi";
-import {
-  Address,
-  NFTCard,
-  ConnectButton,
-  Connector,
-  useAccount,
-} from "@ant-design/web3";
-import { abi } from "./abi";
+import { ConnectButton, Connector, Address, NFTCard } from "@ant-design/web3";
+import { injected, walletConnect } from "wagmi/connectors";
+import { createConfig, http } from "wagmi";
+// ...
 
-const CallTest = () => {
-  const { account } = useAccount();
-  console.log("account:", account);
-  const result = useReadContract({
-    abi,
-    address: "0xB3B263442CF2EfA59A1A83D77ab4725befD22C35",
-    functionName: "balanceOf",
-    args: [account?.address as `0x${string}`],
-  });
+const config = createConfig({
+  chains: [mainnet, sepolia, polygon, hardhat],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+    [polygon.id]: http(),
+    [hardhat.id]: http("http://127.0.0.1:8545/"),
+  },
+  connectors: [
+    injected({
+      target: "metaMask",
+    }),
+    walletConnect({
+      projectId: "c07c0051c2055890eade3556618e38a6",
+      showQrModal: false,
+    }),
+  ],
+});
 
-  const { writeContract } = useWriteContract();
-  useWatchContractEvent({
-    address: "0xB3B263442CF2EfA59A1A83D77ab4725befD22C35",
-    abi,
-    eventName: "Minted",
-    onLogs() {
-      message.success("new minted!");
-      console.log("new minted!");
-    },
-  });
-  return (
-    <div>
-      {result.data?.toString()}
-      <Button
-        onClick={() => {
-          writeContract(
-            {
-              abi,
-              address: "0xB3B263442CF2EfA59A1A83D77ab4725befD22C35",
-              functionName: "init",
-              // args: [BigInt(1)],
-              // value: parseEther("0.01"),
-            },
-            {
-              onSuccess: () => {
-                message.success("Init Success");
-              },
-              onError: (err) => {
-                console.log("err:", err);
-                message.error(err.message);
-              },
-            }
-          );
-        }}
-      >
-        init
-      </Button>
-      <Button
-        onClick={() => {
-          writeContract(
-            {
-              abi,
-              address: "0xB3B263442CF2EfA59A1A83D77ab4725befD22C35",
-              functionName: "numberMinted",
-              args: [account?.address as `0x${string}`],
-              // value: parseEther("0.01"),
-            },
-            {
-              onSuccess: (res) => {
-                // message.success("Number Minted:", res);
-                console.log("Number Minted:", res);
-              },
-              onError: (err) => {
-                console.log("err:", err);
-                message.error(err.message);
-              },
-            }
-          );
-        }}
-      >
-        numberMinted
-      </Button>
-      <Button
-        onClick={() => {
-          writeContract(
-            {
-              abi,
-              address: "0xB3B263442CF2EfA59A1A83D77ab4725befD22C35",
-              functionName: "mint",
-              args: [BigInt(1)],
-              value: parseEther("0.01"),
-            },
-            {
-              onSuccess: () => {
-                message.success("Mint Success");
-              },
-              onError: (err) => {
-                console.log("err:", err);
-                message.error(err.message);
-              },
-            }
-          );
-        }}
-      >
-        mint
-      </Button>
-    </div>
-  );
-};
+const contractInfo = [
+  {
+    id: 1,
+    name: "Ethereum",
+    contractAddress: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+  },
+  {
+    id: 5,
+    name: "Sepolia",
+    contractAddress: "0x418325c3979b7f8a17678ec2463a74355bdbe72c",
+  },
+  {
+    id: 137,
+    name: "Polygon",
+    contractAddress: "0x418325c3979b7f8a17678ec2463a74355bdbe72c",
+  },
+  {
+    id: hardhat.id,
+    name: "Hardhat",
+    contractAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3", // 这里需要替换为你本地部署后获得的地址
+  },
+];
+
+// ...
 
 export default function Web3() {
   return (
     <WagmiWeb3ConfigProvider
-      chains={[Mainnet, Localhost, Sepolia]}
-      transports={{
-        [Mainnet.id]: http(
-          "https://api.zan.top/node/v1/eth/mainnet/f54457478f874ba2975e0eb4ee272b2c"
-        ),
-        [Localhost.id]: http("http://127.0.0.1:7545"),
-        [Sepolia.id]: http("https://eth-sepolia.g.alchemy.com/v2/jnqSYFCF95On94D4-FtBwsaCVVDbviRr"),
+      config={config}
+      wallets={[MetaMask(), WalletConnect()]}
+      eip6963={{
+        autoAddInjectedWallets: true,
       }}
-      wallets={[MetaMask()]}
+      chains={[Sepolia, Polygon, Hardhat]}
     >
       <Address format address="0xEcd0D12E21805803f70de03B72B1C162dB0898d9" />
       <NFTCard
@@ -144,7 +74,7 @@ export default function Web3() {
       <Connector>
         <ConnectButton />
       </Connector>
-      <CallTest />
+      {/* <CallTest /> */}
     </WagmiWeb3ConfigProvider>
   );
 }
